f355ff1667dcdfbe872a8d718bfc257fdbfe8fba
Sri Ram <k.sriramsagar@gmail.com>
2018-10-14 05:44:14 -0400

    localhost

--- app/appclaifai/model/helpers.py
+++ app/appclaifai/model/helpers.py
@@ -1,12 +1,12 @@
 import clarifai
 
-def get_model(app_obj:object , model_name: str) -> object:
+def get_model(app_obj , model_name):
     " Returns a clarifai model."
     if isinstance(model_name, str):
         return app_obj.models.get(model_name)
     return None
 
-def predict(model_obj: object, image_file: str) -> list:
+def predict(model_obj, image_file):
     " Predict the images and returns all the class"
     image_bin = open(image_file, 'rb').read()
     json_obj = model_obj.predict_by_bytes(image_bin)
--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
+import os
+from flask import Flask, render_template, make_response
+from flask import redirect, request, jsonify, url_for
+
 from clarifai.rest import ClarifaiApp
 from appclaifai.model.helpers import get_model
 from appclaifai.model.helpers import predict
 
 app_obj = ClarifaiApp(api_key = 'a28d6ad6382d434a9cae46a67b97364e')
-
-color_model = get_model(app_obj, 'Color')
 paper_model = get_model(app_obj, 'general-v1.3')
 
-output = paper_model.predict_by_bytes(open('appclaifai/pics/ColorPaper1.jpeg', 'rb').read())
+app = Flask(__name__)
+app.secret_key = 's3cr3t'
+app.debug = True
+app._static_folder = os.path.abspath("pics")
+
+@app.route('/', methods=['GET'])
+def home():
+    return redirect('https://srash.netlify.com')
+
+@app.route('/<base64image>', methods=['POST'])
+def captured(base64image):
+    class_probs = predict(paper_model, base64image)
+
+if __name__ == '__main__':
+    app.run()
